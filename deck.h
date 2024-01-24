250c87020911d81101b7d39518428ee139ac1fbe
Alogyn <mohamed.derfoufi.dev@gmail.com>
2024-01-24 05:25:17 -0800

    Prototypes functions & Structures

--- deck.h
+++ deck.h
@@ -6,12 +6,20 @@
 #include <stdio.h>
 #include <string.h>
 
+/**
+ * enum kind_e - Represents the suits of a standard playing card deck.
+ *
+ * @SPADE: Spade suit
+ * @HEART: Heart suit
+ * @CLUB: Club suit
+ * @DIAMOND: Diamond suit
+ */
 typedef enum kind_e
 {
-    SPADE = 0,
-    HEART,
-    CLUB,
-    DIAMOND
+	SPADE = 0,
+	HEART,
+	CLUB,
+	DIAMOND
 } kind_t;
 
 /**
@@ -23,8 +31,8 @@ typedef enum kind_e
  */
 typedef struct card_s
 {
-    const char *value;
-    const kind_t kind;
+	const char *value;
+	const kind_t kind;
 } card_t;
 
 /**
@@ -36,9 +44,9 @@ typedef struct card_s
  */
 typedef struct deck_node_s
 {
-    const card_t *card;
-    struct deck_node_s *prev;
-    struct deck_node_s *next;
+	const card_t *card;
+	struct deck_node_s *prev;
+	struct deck_node_s *next;
 } deck_node_t;
 
 /* Task 12. Dealer (Advanced) */
