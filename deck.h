bdff4548da99136d8f3fbe399d1021ccd54f2797
Mohamed Derfoufi <138708102+Alogyn@users.noreply.github.com>
2024-01-24 02:03:35 +0100

    Prototypes functions & Structures

--- deck.h
+++ deck.h
@@ -43,6 +43,9 @@ typedef struct deck_node_s
 
 /* Task 12. Dealer (Advanced) */
 void sort_deck(deck_node_t **deck);
-int compare_cards(const void *card1, const void *card2);
+int _strcmp(const char *s1, const char *s2);
+char get_value(deck_node_t *card);
+void insertion_sort_deck_kind(deck_node_t **deck);
+void insertion_sort_deck_value(deck_node_t **deck);
 
 #endif /* DECK_H */
