6c8b97801828f86c2140ab376d74541171431eda
Alogyn <mohamed.derfoufi.dev@gmail.com>
2024-01-23 16:06:10 -0800

    Sort an array of integers in ascending order using the Bitonic sort algorithm

--- sort.h
+++ sort.h
@@ -65,7 +65,7 @@ void radix_sort(int *array, size_t size);
 int getMax(int *array, size_t size);
 void countingSort(int *array, size_t size, int exp);
 
-/* Task */
+/* Task 10. Bitonic sort (Advanced) */
 void bitonic_sort(int *array, size_t size);
 
 #endif /* SORT_H */
